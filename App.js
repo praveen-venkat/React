1624d35b4658fc24167281d031c978114b5af29a
Praveen Venkat <praveen25494@gmail.com>
2023-07-23 12:56:28 +0530

    Rendering sibling child using React example

--- App.js
+++ App.js
@@ -2,12 +2,16 @@
  * <div>
  *  <div>
  *      <h1> Welcome to react </h1> 
+ *      <h2> Sibling child </h2>
  *  <div>
  * </div>
  * 
  */
 
-const child = React.createElement('div',{id:"parent"},React.createElement('div',{id:"child"},React.createElement('h1',{id: "header"},'This is the header')));
+const child = React.createElement
+('div',{id:"parent"},
+React.createElement('div',{id:"child"},
+[React.createElement('h1',{id: "header"},'This is the header'),React.createElement('h2',{},"This is sibling child")]));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
